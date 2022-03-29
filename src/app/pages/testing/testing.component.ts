b7bf66cc11f9caf41f8e3c83e4da4579d375a0c1
giou55 <giourmetakis00@gmail.com>
2022-03-29 14:47:50 +0300

    changes

--- src/app/pages/testing/testing.component.ts
+++ src/app/pages/testing/testing.component.ts
@@ -7,36 +7,15 @@ import { Observable, interval, Subscription } from 'rxjs';
   styleUrls: ['./testing.component.css']
 })
 export class TestingComponent implements OnInit {
-  output:Number;  
-  observable:Observable<any>;
-  isSubscribed:Boolean = false;
-  subscription:Subscription;
+  output: Number;
+  observable: Observable<any>;
+  isSubscribed: Boolean = false;
+  subscription: Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
     this.observable = interval(500);
   }
-
-  onSubscribe() {
-      this.isSubscribed = true;
-      this.subscription = this.observable.subscribe(v => this.output = v);
-  }
-
-  onUnsubscribe() {
-    this.isSubscribed = false;
-    this.subscription.unsubscribe();
-}
-
-  // onSubscribe() {
-  //   if(!this.isSubscribed){
-  //     this.subscription = this.observable.subscribe(v => this.output = v);
-  //     this.isSubscribed = true;
-  //   }
-  //   if(this.isSubscribed){
-  //     this.subscription.unsubscribe();
-  //     this.isSubscribed = false;
-  //   }
-  // }
 
 }
