f9fb4719e06619f849c773253061d066a9cf7844
giou55 <giourmetakis00@gmail.com>
2022-04-03 00:44:33 +0300

    working with zip operator

--- src/app/operators/zip/zip.component.ts
+++ src/app/operators/zip/zip.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { zip, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, Subject, zip, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+type Souvlaki = ["pita", "kreas", "ntomata", "kremidi", "patates"];
+
+let pitaCounter = 0;
+let kreasCounter = 0;
+let ntomataCounter = 0;
+let kremidiCounter = 0;
+let patatesCounter = 0;
 
 @Component({
   selector: 'app-zip',
@@ -8,21 +16,36 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./zip.component.css']
 })
 export class ZipComponent implements OnInit {
+  souvlaki$: Observable<Souvlaki>;
+
+  pita = new Subject<'pita'>();
+  kreas = new Subject<'kreas'>();
+  ntomata = new Subject<'ntomata'>();
+  kremidi = new Subject<'kremidi'>();
+  patates = new Subject<'patates'>();
+
+  souvlakiCounter(i: number) {
+    return new Array(i);
+  }
+  counter:number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
-    let ages = of(27, 25, 29);
-    let names = of('Foo', 'Bar', 'Beer');
-    let isDev = of(true, true, false);
 
-    zip(ages, names, isDev)
-      .subscribe(x => console.log(x));
+    this.souvlaki$ = zip(
+      this.pita.pipe(map((ing) => `${ing}${++pitaCounter}`),tap(console.log)),
+      this.kreas.pipe(map((ing) => `${ing}${++kreasCounter}`),tap(console.log)),
+      this.ntomata.pipe(map((ing) => `${ing}${++ntomataCounter}`),tap(console.log)),
+      this.kremidi.pipe(map((ing) => `${ing}${++kremidiCounter}`),tap(console.log)),
+      this.patates.pipe(map((ing) => `${ing}${++patatesCounter}`),tap(console.log))
+    ).pipe(
+      tap((souvlaki) => {
+        console.log('Enjoy!', souvlaki);
+        this.counter = +this.counter + 1;
+      })
+    );
 
-    // Outputs
-    // [ 27, 'Foo', true ]
-    // [ 25, 'Bar', true ]
-    // [ 29, 'Beer', false ]
   }
 
 }
